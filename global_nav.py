921128d9ea991a43bb686b30ab5ab75bf82d0f03
Pierre <pierre.emmel@epfl.ch>
2025-11-25 17:57:59 +0100

    feat: adding diagonal distance

--- global_nav.py
+++ global_nav.py
@@ -8,16 +8,17 @@ from heapq import heappush, heappop
 class GlobalNavigation : 
 
     def __init__(self):
-       
-        
-        # self.vision = vision #To get grid, thymio_pos, thymio_size, cell_size and goal
-        # self.Start = self.vision.thymio_pos
-        # self.Goal = self.vision.goal
-        # self.grid = self.vision.grid
-        # self.cell_size = self.vision.cell_size
-        # self.robot_size = self.vision.thymio_size 
-        # self.robot_c_width = int(self.vision.thymio_size[0] / self.cell_size[0])
-        # self.robot_c_height = int(self.vision.thymio_size[1] / self.cell_size[1])
+
+        self.Start = None
+        self.Goal = None
+        self.grid = None
+        self.cell_size = None
+        self.thymio_size = None
+
+        #Adapt thymio size for grid
+        self.robot_size = None
+        self.robot_size_cells = None
+
         '''
         Testing zone 
         '''
@@ -48,21 +49,43 @@ class GlobalNavigation :
         '''
         End Testing zone
         '''
+
+    def set_gnav(self, vision):
+
+        self.Start = vision.thymio_pos
+        self.Goal = vision.goal
+        self.grid = vision.grid
+        self.cell_size = vision.cell_size
+        self.thymio_size = vision.thymio_size
+
+        #Adapt thymio size for grid
+        self.robot_size = int(np.ceil(self.thymio_size / self.cell_size)) // 2 #np.ceil to take he superior int
+        self.robot_size_cells = 2 * self.robot_size + 1  # Rayon à gauche + centre + rayon à droite
+
+        '''
+        Testing
+        '''
+        print("Rayon du robot en cellules :", self.robot_size)
+        print("Taille totale du robot en cellules :", self.robot_size_cells)
+        '''
+        Stop Testing
+        '''
+
+    
     
     #Function to verify if the robot is inside the grid and not on an obstacle.
-    def verification(self, position):
-        
-        x, y = position #Top left position
-        
-    
-        for i in range(x, x + self.robot_c_height):
-            for j in range(y, y + self.robot_c_width):
+    def verification(self):
+        col, row = self.Start
+        print("Ce qu'on check à droite : ", -self.robot_size + col)
+        print("Ce qu'on check à gauche : ", self.robot_size + col)
+        for i in range(-self.robot_size + row, row + self.robot_size):
+            for j in range(-self.robot_size + col, col + self.robot_size):
                 
                 if i >= len(self.grid) or j >= len(self.grid[0]):
-                    print("Le robot dépasse les limites de la grille.")
+                    print("Error : Thymio is not inside the grid !")
                     return False
                 elif self.grid[i][j] == 0 :
-                    print("Erreur : Le robot touche un mur.")
+                    print("Error : Thymio is on an obstable !")
                     return False
 
         return True
@@ -83,7 +106,6 @@ class GlobalNavigation :
                     for di in range(-self.robot_size, self.robot_size+1):
                         for dj in range(-self.robot_size, self.robot_size+1):
                             
-                            print("Distance di selon x : ", di)
                             ni = i + di
                             nj = j + dj
                             if 0 <= ni < len(self.grid) and 0 <= nj < len(self.grid[0]):
@@ -91,15 +113,26 @@ class GlobalNavigation :
 
         self.grid = modified_grid
 
-    def heuristic(self, a, b):
-        # Implement the Manhattan distance heuristic
-        return abs(a[0] - b[0]) + abs(a[1] - b[1])
+    # def heuristic(self, a, b):
+    #     # Implement the Manhattan distance heuristic
+    #     return abs(a[0] - b[0]) + abs(a[1] - b[1])
+    
+    #Compute the diagonal heuristic
+    def heuristic(self, a, b) : 
+
+        D = 1
+        D2 = np.sqrt(2)
+        da = abs(a[0] - b[0]) 
+        db = abs(a[1] - b[1])
+        
+        return D*(da + db) + (D2 - 2*D)*min(da,db)
 
     #Path finding algorithme with A* and Manhattan distance 
     def grid_search(self): 
         
         #Function to adapt the path for thymio robot dimension
         self.growing_obstacles()
+        a = self.verification()
         ## initialize the variables above
         came_from = {}      # to reconstruct path
         g_costs = {self.Start: 0}    # cost from start to the cell
@@ -117,25 +150,40 @@ class GlobalNavigation :
             if current_pos == self.Goal: # if Goal is marked then stop the algorithme
                 break
 
-            # Get neighbors (up, down, left, right) -> be careful it is like a matrix
+            # Get neighbors -> be careful it is like a matrix
             neighbors = [
-                (current_pos[0]-1, current_pos[1]),  # Up
-                (current_pos[0]+1, current_pos[1]),  # Down
-                (current_pos[0], current_pos[1]-1),  # Left
-                (current_pos[0], current_pos[1]+1)   # Right
+                (current_pos[0]-1, current_pos[1]),   #Up
+                (current_pos[0]+1, current_pos[1]),   #Down
+                (current_pos[0], current_pos[1]-1),   #Left
+                (current_pos[0], current_pos[1]+1),   #Right
+                (current_pos[0]-1, current_pos[1]-1), #Diagonal Up-Left
+                (current_pos[0]-1, current_pos[1]+1), #Diagonal Up-Right
+                (current_pos[0]+1, current_pos[1]-1), #Diagonal Down-Left
+                (current_pos[0]+1, current_pos[1]+1)  #Diagonal Down-Right
             ]
 
             for neighbor in neighbors: # for each neighbor of marked cells
                 # Check if neighbor is within bounds
                 if (0 <= neighbor[0] < len(self.grid)) and (0 <= neighbor[1] < self.grid.shape[1]): #check if the neighbor is less than 1 and greater than 0
+                    
                     # Check if neighbor is not an obstacle
-
-                    if self.grid[neighbor[0], neighbor[1]] != 0:
+                    if self.grid[neighbor[0], neighbor[1]] != 0 :
 
                         if neighbor in explored:
                             continue
 
-                        tentative_g_cost = current_g_cost + self.grid[neighbor[0], neighbor[1]]
+                        if (abs(neighbor[0] - current_pos[0]) == 1 and abs(neighbor[1] - current_pos[1]) == 1) :
+                            is_diagonal = True 
+                        else : 
+                            is_diagonal = False 
+
+                        if is_diagonal : 
+                            move_cost = np.sqrt(2) #Diagonal cost is sqrt(2)
+                        else : 
+                            move_cost = 1 #Orthogonal cost is 1 
+                        
+                        tentative_g_cost = current_g_cost + move_cost
+                        #tentative_g_cost = current_g_cost + self.grid[neighbor[0], neighbor[1]]
 
                         if neighbor not in g_costs or tentative_g_cost < g_costs[neighbor]:
                             g_costs[neighbor] = tentative_g_cost
